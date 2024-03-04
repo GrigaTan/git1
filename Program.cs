a5922213f835c21ae143ea111750bd1142ce4eee
Дмитрий Стефанив <stefaniv.d@edu.narfu.ru>
2024-03-05 00:07:03 +0300

    tap.ome

--- Program.cs
+++ Program.cs
@@ -9,34 +9,3 @@ void array(string[] stringArray)
     stringArray[i] = Console.ReadLine();
     }
 }
-string[] sym(string[] stringArray)
-{
-    int n = 0;
-    for (int i = 0; i < stringArray.Length; i++)
-    {
-        if (stringArray[i].Length <= 2)
-            n++;
-    }
-    string[] rez = new string[n];
-    int j = 0;
-    for (int i = 0; i < stringArray.Length; i++)
-    {
-        if (stringArray[i].Length <= 2)
-        {
-            rez[j] = stringArray[i];
-            j++;
-        }
-    }
-    return rez;
-}
-void printA(string[] stringArray)
-{
-    Console.Write("[");
-    for (int i = 0; i < stringArray.Length; i++)
-    {
-        Console.Write($"‘{ stringArray[i]}’, ");
-}
-Console.Write("]");
-}
-array(stringArray);
-printA(sym(stringArray));
